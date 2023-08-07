35bea54e4f4fc38cc46207c02c6d5fc7c7b23db3
Omar Fathy <91073589+OmarFathy22@users.noreply.github.com>
2023-08-07 03:10:22 +0300

    new

--- src/components/Projects/ProjectsData.jsx
+++ src/components/Projects/ProjectsData.jsx
@@ -1,63 +1,72 @@
- import gym from "../../assets/cardImage/gym.png";
- import memory from "../../assets/cardImage/memory_card.png";
-//  import bootcamp from "../../assets/cardImage/bootcamb.png";
- import portofolio from "../../assets/cardImage/portofolio3d.png";
- import taskati from "../../assets/cardImage/taskati.png";
- import connectivea from "../../assets/cardImage/connectivea.png";
-
-export const buttons = ["ALL", "VANILLA JS" , "REACT"];
+export const buttons = ["ALL", "VANILLA JS", "REACT"];
 export const ALL = [
   {
-    image: connectivea,
+    name: "Kizerr",
+    description: "Fiverr clone built with MERN stack and Tailwind CSS",
+    stack: [
+      "MERN STACK",
+      "MongoDB",
+      "Express",
+      "React",
+      "Node.js",
+      "Tailwind CSS",
+    ],
+    SourceCode: "https://github.com/OmarFathy22/Kizerr-project",
+    LiveDemo: "https://kizerr.pages.dev",
+    video:
+      "https://firebasestorage.googleapis.com/v0/b/postatiwith.appspot.com/o/Portofolio%20website%20videos%2FConnectivea.mp4?alt=media&token=3db91fe2-f311-4482-8310-3ff4ed67fbfa&_gl=1*1kajvhq*_ga*MjE0NDEzNDExNy4xNjg0MDMyMTIy*_ga_CW55HF8NVT*MTY4NTczNTU2MC40Mi4xLjE2ODU3MzY4MTcuMC4wLjA",
+    youtubeId: "X9VEFsVZrmk",
+  },
+  {
     name: "CONNECTIVEA",
-    description:"REACT-BASED SOCIAL MEDIA APP FOR EASY CONNECTIONS",
+    description: "REACT-BASED SOCIAL MEDIA APP FOR EASY CONNECTIONS",
     stack: ["MUI", "React", "Firebase"],
     SourceCode: "https://github.com/OmarFathy22/muiproject2-master",
     LiveDemo: "https://connectiveaa.web.app",
-    video:"https://firebasestorage.googleapis.com/v0/b/postatiwith.appspot.com/o/Portofolio%20website%20videos%2FConnectivea.mp4?alt=media&token=3db91fe2-f311-4482-8310-3ff4ed67fbfa&_gl=1*1kajvhq*_ga*MjE0NDEzNDExNy4xNjg0MDMyMTIy*_ga_CW55HF8NVT*MTY4NTczNTU2MC40Mi4xLjE2ODU3MzY4MTcuMC4wLjA"
-    ,youtubeId: "q_Bo7OwHkpQ"
-
+    video:
+      "https://firebasestorage.googleapis.com/v0/b/postatiwith.appspot.com/o/Portofolio%20website%20videos%2FConnectivea.mp4?alt=media&token=3db91fe2-f311-4482-8310-3ff4ed67fbfa&_gl=1*1kajvhq*_ga*MjE0NDEzNDExNy4xNjg0MDMyMTIy*_ga_CW55HF8NVT*MTY4NTczNTU2MC40Mi4xLjE2ODU3MzY4MTcuMC4wLjA",
+    youtubeId: "q_Bo7OwHkpQ",
   },
   {
-    image: taskati,
     name: "TASKATI",
-    description:  "REACT APP FOR EASY TASKS ORGANIZATION AND TRACING",
-    stack: ["Fontawesome", "Firebase", "React","CSS"],
+    description: "REACT APP FOR EASY TASKS ORGANIZATION AND TRACING",
+    stack: ["Fontawesome", "Firebase", "React", "CSS"],
     SourceCode: "https://github.com/OmarFathy22/TAKATI",
     LiveDemo: "https://taskati.pages.dev/",
-    video: "https://firebasestorage.googleapis.com/v0/b/postatiwith.appspot.com/o/Portofolio%20website%20videos%2FTaskati.mp4?alt=media&token=8a6c135a-f004-496e-a2a6-9aaeaa02e27c"
-    ,youtubeId: "RlSml970YCM"
+    video:
+      "https://firebasestorage.googleapis.com/v0/b/postatiwith.appspot.com/o/Portofolio%20website%20videos%2FTaskati.mp4?alt=media&token=8a6c135a-f004-496e-a2a6-9aaeaa02e27c",
+    youtubeId: "RlSml970YCM",
   },
   {
-    image: portofolio,
     name: "3D Portfolio App",
-    description:"PORTFOLIO APP WITH SOME COOL 3D MODELS",
+    description: "PORTFOLIO APP WITH SOME COOL 3D MODELS",
     stack: ["Styled-Components", "React", "Three.js"],
     SourceCode: "https://github.com/OmarFathy22/3D-portofolio",
     LiveDemo: "https://3d-portofolio.pages.dev",
-    video: "https://firebasestorage.googleapis.com/v0/b/postatiwith.appspot.com/o/Portofolio%20website%20videos%2FUntitled%20video%20-%20Made%20with%20Clipchamp%20(3).mp4?alt=media&token=37a6b2bd-ca7a-4307-b10b-c52bb59e9a08"
-    ,youtubeId: "oE05bOgNNNI"
+    video:
+      "https://firebasestorage.googleapis.com/v0/b/postatiwith.appspot.com/o/Portofolio%20website%20videos%2FUntitled%20video%20-%20Made%20with%20Clipchamp%20(3).mp4?alt=media&token=37a6b2bd-ca7a-4307-b10b-c52bb59e9a08",
+    youtubeId: "oE05bOgNNNI",
   },
   {
-    image: gym,
     name: "Way To Forma",
-    description:  "WORKOUT APP BUILT WITH REACT TO SHOWCASE THE BEST EXERCISES",
-    stack: ["Tailwind CSS",  "Firebase" , "React" , "API"],
+    description: "WORKOUT APP BUILT WITH REACT TO SHOWCASE THE BEST EXERCISES",
+    stack: ["Tailwind CSS", "Firebase", "React", "API"],
     SourceCode: "https://github.com/OmarFathy22/dream-gym-clone-react",
     LiveDemo: "https://way-to-forma.web.app/",
-    video:"https://firebasestorage.googleapis.com/v0/b/postatiwith.appspot.com/o/Portofolio%20website%20videos%2Fway%20to%20forma.mp4?alt=media&token=580317a5-a2e4-4a2a-9ea1-994136724489",
-    youtubeId: "RjkK7wwZT-k"
-
+    video:
+      "https://firebasestorage.googleapis.com/v0/b/postatiwith.appspot.com/o/Portofolio%20website%20videos%2Fway%20to%20forma.mp4?alt=media&token=580317a5-a2e4-4a2a-9ea1-994136724489",
+    youtubeId: "RjkK7wwZT-k",
   },
   {
-    image: memory,
-    name: 'Memory Card Game',
-    description:"  SIMPLE COOL GAME FOR TESTING YOUR MEMORY MEMORY CARD GAME  SKILLS AND TO HAVE SOME FUN",
-    stack: ['HTML', 'CSS', 'JS'],
-    SourceCode: 'https://github.com/OmarFathy22/Memory_card_GameV2',
-    LiveDemo: 'https://omarfathy22.github.io/Memory_Card_Game/',
-    video: "https://firebasestorage.googleapis.com/v0/b/postatiwith.appspot.com/o/Portofolio%20website%20videos%2FMemory%20Card%20Game.mp4?alt=media&token=096da0d0-f6c7-43ac-8cad-834ddff6c5e6"
-    ,youtubeId: "k3K7-hI0tTk"
+    name: "Memory Card Game",
+    description:
+      "  SIMPLE COOL GAME FOR TESTING YOUR MEMORY MEMORY CARD GAME  SKILLS AND TO HAVE SOME FUN",
+    stack: ["HTML", "CSS", "JS"],
+    SourceCode: "https://github.com/OmarFathy22/Memory_card_GameV2",
+    LiveDemo: "https://omarfathy22.github.io/Memory_Card_Game/",
+    video:
+      "https://firebasestorage.googleapis.com/v0/b/postatiwith.appspot.com/o/Portofolio%20website%20videos%2FMemory%20Card%20Game.mp4?alt=media&token=096da0d0-f6c7-43ac-8cad-834ddff6c5e6",
+    youtubeId: "k3K7-hI0tTk",
   },
   // {
   //   image: bootcamp,
@@ -71,14 +80,15 @@ export const ALL = [
 ];
 export const VANILLA_JS = [
   {
-    image: memory,
-    name: 'Memory Card Game',
-    description: 'SIMPLE COOL GAME FOR TESTING YOUR MEMORY SKILLS AND TO HAVE SOME FUN',
-    stack: ['HTML', 'CSS', 'JS'],
-    SourceCode: 'https://github.com/OmarFathy22/Memory_Card_Game',
-    LiveDemo: 'https://omarfathy22.github.io/Memory_Card_Game/',
-    video:"https://firebasestorage.googleapis.com/v0/b/postatiwith.appspot.com/o/Portofolio%20website%20videos%2FMemory%20Card%20Game.mp4?alt=media&token=096da0d0-f6c7-43ac-8cad-834ddff6c5e6"
-    ,youtubeId: "k3K7-hI0tTk"
+    name: "Memory Card Game",
+    description:
+      "SIMPLE COOL GAME FOR TESTING YOUR MEMORY SKILLS AND TO HAVE SOME FUN",
+    stack: ["HTML", "CSS", "JS"],
+    SourceCode: "https://github.com/OmarFathy22/Memory_Card_Game",
+    LiveDemo: "https://omarfathy22.github.io/Memory_Card_Game/",
+    video:
+      "https://firebasestorage.googleapis.com/v0/b/postatiwith.appspot.com/o/Portofolio%20website%20videos%2FMemory%20Card%20Game.mp4?alt=media&token=096da0d0-f6c7-43ac-8cad-834ddff6c5e6",
+    youtubeId: "k3K7-hI0tTk",
   },
   // {
   //   image: bootcamp,
@@ -91,55 +101,69 @@ export const VANILLA_JS = [
 ];
 export const REACT = [
   {
-    image: taskati,
-    name: "TASKATI",
-    description:  "REACT APP FOR EASY TASKS ORGANIZATION AND TRACING",
-    stack: ["Fontawesome", "Firebase", "React","CSS"],
-    SourceCode: "https://picsum.photos/200/300",
-    LiveDemo: "https://taskati.pages.dev/",
-    video:"https://firebasestorage.googleapis.com/v0/b/postatiwith.appspot.com/o/Portofolio%20website%20videos%2FTaskati.mp4?alt=media&token=8a6c135a-f004-496e-a2a6-9aaeaa02e27c"
-    ,youtubeId: "RlSml970YCM"
+    name: "Kizerr",
+    description: "Fiverr clone built with MERN stack and Tailwind CSS",
+    stack: [
+      "MERN STACK",
+      "MongoDB",
+      "Express",
+      "React",
+      "Node.js",
+      "Tailwind CSS",
+    ],
+    SourceCode: "https://github.com/OmarFathy22/Kizerr-project",
+    LiveDemo: "https://kizerr.pages.dev",
+    video:
+      "https://firebasestorage.googleapis.com/v0/b/postatiwith.appspot.com/o/Portofolio%20website%20videos%2FConnectivea.mp4?alt=media&token=3db91fe2-f311-4482-8310-3ff4ed67fbfa&_gl=1*1kajvhq*_ga*MjE0NDEzNDExNy4xNjg0MDMyMTIy*_ga_CW55HF8NVT*MTY4NTczNTU2MC40Mi4xLjE2ODU3MzY4MTcuMC4wLjA",
+    youtubeId: "X9VEFsVZrmk",
   },
   {
-    image: connectivea,
+    name: "TASKATI",
+    description: "REACT APP FOR EASY TASKS ORGANIZATION AND TRACING",
+    stack: ["Fontawesome", "Firebase", "React", "CSS"],
+    SourceCode: "https://picsum.photos/200/300",
+    LiveDemo: "https://taskati.pages.dev/",
+    video:
+      "https://firebasestorage.googleapis.com/v0/b/postatiwith.appspot.com/o/Portofolio%20website%20videos%2FTaskati.mp4?alt=media&token=8a6c135a-f004-496e-a2a6-9aaeaa02e27c",
+    youtubeId: "RlSml970YCM",
+  },
+  {
     name: "Posts App",
-    description:"REACT-BASED SOCIAL MEDIA APP FOR EASY CONNECTIONS",
+    description: "REACT-BASED SOCIAL MEDIA APP FOR EASY CONNECTIONS",
     stack: ["MUI", "React", "Firebase"],
     SourceCode: "https://picsum.photos/200/300",
     LiveDemo: "https://postati.netlify.app/",
-    video:"https://firebasestorage.googleapis.com/v0/b/postatiwith.appspot.com/o/Portofolio%20website%20videos%2FConnectivea.mp4?alt=media&token=3db91fe2-f311-4482-8310-3ff4ed67fbfa&_gl=1*1kajvhq*_ga*MjE0NDEzNDExNy4xNjg0MDMyMTIy*_ga_CW55HF8NVT*MTY4NTczNTU2MC40Mi4xLjE2ODU3MzY4MTcuMC4wLjA"
-    ,youtubeId: "q_Bo7OwHkpQ"
-
+    video:
+      "https://firebasestorage.googleapis.com/v0/b/postatiwith.appspot.com/o/Portofolio%20website%20videos%2FConnectivea.mp4?alt=media&token=3db91fe2-f311-4482-8310-3ff4ed67fbfa&_gl=1*1kajvhq*_ga*MjE0NDEzNDExNy4xNjg0MDMyMTIy*_ga_CW55HF8NVT*MTY4NTczNTU2MC40Mi4xLjE2ODU3MzY4MTcuMC4wLjA",
+    youtubeId: "q_Bo7OwHkpQ",
   },
   {
-    image: portofolio,
     name: "Portfolio 3D App",
-    description:"PORTFOLIO APP WITH SOME COOL 3D MODELS",
+    description: "PORTFOLIO APP WITH SOME COOL 3D MODELS",
     stack: ["Styled-Components", "React", "Three.js"],
     SourceCode: "https://picsum.photos/200/300",
     LiveDemo: "https://portofolio-morata.web.app/",
-    video:  "https://firebasestorage.googleapis.com/v0/b/postatiwith.appspot.com/o/Portofolio%20website%20videos%2FUntitled%20video%20-%20Made%20with%20Clipchamp%20(3).mp4?alt=media&token=37a6b2bd-ca7a-4307-b10b-c52bb59e9a08"
-    ,youtubeId: "oE05bOgNNNI"
-
+    video:
+      "https://firebasestorage.googleapis.com/v0/b/postatiwith.appspot.com/o/Portofolio%20website%20videos%2FUntitled%20video%20-%20Made%20with%20Clipchamp%20(3).mp4?alt=media&token=37a6b2bd-ca7a-4307-b10b-c52bb59e9a08",
+    youtubeId: "oE05bOgNNNI",
   },
   {
-    image: gym,
     name: "Gym Exercises App",
-    description:  "WORKOUT APP BUILT WITH REACT TO SHOWCASE THE BEST EXERCISES",
-    stack: ["Tailwind CSS",  "Firebase" , "React" , "API"],
+    description: "WORKOUT APP BUILT WITH REACT TO SHOWCASE THE BEST EXERCISES",
+    stack: ["Tailwind CSS", "Firebase", "React", "API"],
     SourceCode: "https://github.com/OmarFathy22/dream-gym-clone-react",
     LiveDemo: "https://way-to-forma.web.app/",
-    video:"https://firebasestorage.googleapis.com/v0/b/postatiwith.appspot.com/o/Portofolio%20website%20videos%2Fway%20to%20forma.mp4?alt=media&token=580317a5-a2e4-4a2a-9ea1-994136724489"
-    ,youtubeId: "RjkK7wwZT-k"
+    video:
+      "https://firebasestorage.googleapis.com/v0/b/postatiwith.appspot.com/o/Portofolio%20website%20videos%2Fway%20to%20forma.mp4?alt=media&token=580317a5-a2e4-4a2a-9ea1-994136724489",
+    youtubeId: "RjkK7wwZT-k",
   },
 ];
 export const NEXT = [
   {
-    image: gym,
     name: "Gym Exercises App",
-    description:  "WORKOUT APP BUILT WITH REACT TO SHOWCASE THE BEST EXERCISES",
-    stack: ["Tailwind CSS",  "Firebase" , "Next.js" , "API"],
+    description: "WORKOUT APP BUILT WITH REACT TO SHOWCASE THE BEST EXERCISES",
+    stack: ["Tailwind CSS", "Firebase", "Next.js", "API"],
     SourceCode: "https://github.com/OmarFathy22/my-dream-gym",
-    LiveDemo: "https://way-to-forma.vercel.app/"
+    LiveDemo: "https://way-to-forma.vercel.app/",
   },
 ];
